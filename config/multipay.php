d8680c31dbd3cf10f9769f787c0721edad41e8e9
bolivir <mosselmanricardo@gmail.com>
2023-01-22 13:24:58 +0100

    * Initial commit

--- config/multipay.php
+++ config/multipay.php
@@ -1,14 +1,34 @@
 <?php
 
 return [
-    'default_provider' => null,
+    'default_provider' => 'paypal',
     'providers' => [
         'paypal' => [
-            'api_key' => '',
-            'mode' => '',
+            'client_id' => '',
+            'secret' => '',
+            'mode' => 'sandbox',
+            'currency' => 'EUR',
+            'return_url' => '',
+            'cancel_url' => '',
         ],
         'mollie' => [
-            'api_key',
+            'api_key' => '',
         ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Maps
+    |--------------------------------------------------------------------------
+    |
+    | This is the array of Classes that maps to Drivers above.
+    | You can create your own driver if you like and add the
+    | config in the drivers array and the class to use for
+    | here with the same name. You will have to extend
+    |  Bolivir\Multipay\Abstracts\Driver in your driver.
+    |
+    */
+    'map' => [
+        'paypal' => \Bolivir\Multipay\Drivers\Paypal\Paypal::class,
     ],
 ];
